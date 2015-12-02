127707acd9541a8b2696f33e22942b98bfa5640b
zancas <j.wilcox@f5.com>
2015-12-02 16:46:38 -0500

    Fix PEP8 style issues.

--- icontrolREST/test/test_session.py
+++ icontrolREST/test/test_session.py
@@ -1,6 +1,8 @@
 import mock
 import pytest
-from .. import session
+
+from icontrolREST import session
+
 
 @pytest.fixture()
 def ICRS():
@@ -17,6 +19,7 @@ def ICRS():
     fake_ICRS.session.put.return_value = mock_response
     return fake_ICRS
 
+
 @pytest.fixture()
 def uparts():
     parts_dict = {'bigip_icr_uri': 'https://0.0.0.0/mgmt/tm/',
@@ -26,12 +29,13 @@ def uparts():
                   'suffix': '/members/m1'}
     return parts_dict
 
-#Test uri component validation
+# Test uri component validation
 def test_incorrect_uri_construction_bad_scheme(uparts):
     uparts['bigip_icr_uri'] = 'hryttps://0.0.0.0/mgmt/tm/'
     with pytest.raises(session.InvalidScheme) as IS:
         session.generate_bigip_uri(**uparts)
     assert IS.value.message == 'hryttps'
+
 
 def test_incorrect_uri_construction_bad_mgmt_path(uparts):
     uparts['bigip_icr_uri'] = 'https://0.0.0.0/magmt/tm/'
@@ -39,12 +43,14 @@ def test_incorrect_uri_construction_bad_mgmt_path(uparts):
         session.generate_bigip_uri(**uparts)
     assert IR.value.message == '/magmt/tm/'
 
+
 def test_incorrect_uri_construction_bad_base_nonslash_last(uparts):
     uparts['bigip_icr_uri'] = 'https://0.0.0.0/mgmt/tm'
     with pytest.raises(session.InvalidBigIP_ICRURI) as IR:
         session.generate_bigip_uri(**uparts)
     test_value = "The bigip_icr_uri must end with '/'!!  But it's: /mgmt/tm"
     assert IR.value.message == test_value
+
 
 def test_incorrect_uri_construction_bad_prefix_collection_wrong_startswith(uparts):
     uparts['prefix_collections'] = '/actions/bar/'
@@ -55,11 +61,13 @@ def test_incorrect_uri_construction_bad_prefix_collection_wrong_startswith(upart
     % uparts['prefix_collections']
     assert IR.value.message == test_value
 
+
 def test_incorrect_uri_construction_bad_prefix_collection_wrong_root_collection(uparts):
     uparts['prefix_collections'] = 'foo/bar/'
     with pytest.raises(session.InvalidPrefixCollection) as IR:
         session.generate_bigip_uri(**uparts)
     assert IR.value.message == "foo is not in the list of root collections: ['actions', 'analytics', 'apm', 'asm', 'auth', 'cli', 'cm', 'gtm', 'ltm', 'net', 'pem', 'security', 'sys', 'transaction', 'util', 'vcmp', 'wam', 'wom']"
+
 
 def test_incorrect_uri_construction_bad_prefix_collection_wrong_endswith(uparts):
     uparts['prefix_collections'] = 'actions/bar'
@@ -70,11 +78,13 @@ def test_incorrect_uri_construction_bad_prefix_collection_wrong_endswith(uparts)
     % uparts['prefix_collections']
     assert IR.value.message == test_value
 
+
 def test_incorrect_uri_construction_illegal_slash_folder_char(uparts):
     uparts['folder'] = 'spam/ham'
     with pytest.raises(session.InvalidInstanceNameOrFolder) as II:
         session.generate_bigip_uri(**uparts)
     assert II.value.message == "instance names and folders cannot contain '/', but it's: spam/ham"
+
 
 def test_incorrect_uri_construction_illegal_tilde_folder_char(uparts):
     uparts['folder'] = 'spam~ham'
@@ -82,11 +92,13 @@ def test_incorrect_uri_construction_illegal_tilde_folder_char(uparts):
         session.generate_bigip_uri(**uparts)
     assert II.value.message == "instance names and folders cannot contain '~', but it's: spam~ham"
 
+
 def test_incorrect_uri_construction_illegal_suffix_nonslash_first(uparts):
     uparts['suffix'] = 'ham'
     with pytest.raises(session.InvalidSuffixCollection) as II:
         session.generate_bigip_uri(**uparts)
     assert II.value.message == "suffix_collections path element must start with '/', but it's: ham"
+
 
 def test_incorrect_uri_construction_illegal_suffix_slash_last(uparts):
     uparts['suffix'] = '/ham/'
@@ -94,22 +106,26 @@ def test_incorrect_uri_construction_illegal_suffix_slash_last(uparts):
         session.generate_bigip_uri(**uparts)
     assert II.value.message == "suffix_collections path element must not end with '/', but it's: /ham/"
 
-#Test uri construction
+
+# Test uri construction
 def test_correct_uri_construction_folderless(uparts):
     uparts['folder'] = ''
     uri = session.generate_bigip_uri(**uparts)
     assert uri == 'https://0.0.0.0/mgmt/tm/ltm/bar/~foobar1/members/m1'
+
 
 def test_correct_uri_construction_nameless(uparts):
     uparts['instance_name'] = ''
     uri = session.generate_bigip_uri(**uparts)
     assert uri == "https://0.0.0.0/mgmt/tm/ltm/bar/~BIGCUSTOMER/members/m1"
 
+
 def test_correct_uri_construction_folderless_and_nameless(uparts):
     uparts['folder'] = ''
     uparts['instance_name'] = ''
     uri = session.generate_bigip_uri(**uparts)
     assert uri == "https://0.0.0.0/mgmt/tm/ltm/bar/members/m1"
+
 
 def test_correct_uri_construction_folderless_and_nameless_and_suffixless(uparts):
     uparts['folder'] = ''
@@ -118,11 +134,13 @@ def test_correct_uri_construction_folderless_and_nameless_and_suffixless(uparts)
     uri = session.generate_bigip_uri(**uparts)
     assert uri == "https://0.0.0.0/mgmt/tm/ltm/bar/"
 
+
 def test_correct_uri_construction_folderless_and_suffixless(uparts):
     uparts['folder'] = ''
     uparts['suffix'] = ''
     uri = session.generate_bigip_uri(**uparts)
     assert uri == 'https://0.0.0.0/mgmt/tm/ltm/bar/~foobar1'
+
 
 def test_correct_uri_construction_nameless_and_suffixless(uparts):
     uparts['instance_name'] = ''
@@ -130,9 +148,11 @@ def test_correct_uri_construction_nameless_and_suffixless(uparts):
     uri = session.generate_bigip_uri(**uparts)
     assert uri == 'https://0.0.0.0/mgmt/tm/ltm/bar/~BIGCUSTOMER'
 
-#Test exception handling
+
+# Test exception handling
 def test_wrapped_delete_success(ICRS):
     response_obj = ICRS.delete('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
+
 
 def test_wrapped_delete_207_fail(ICRS):
     ICRS.session.delete.return_value.status_code = 207
@@ -140,8 +160,10 @@ def test_wrapped_delete_207_fail(ICRS):
         response_obj = ICRS.delete('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
     assert CHE.value.message.startswith('207 Unexpected Error: ')
 
+
 def test_wrapped_get_success(ICRS):
     response_obj = ICRS.get('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
+
 
 def test_wrapped_get_207_fail(ICRS):
     ICRS.session.get.return_value.status_code = 207
@@ -149,8 +171,10 @@ def test_wrapped_get_207_fail(ICRS):
         response_obj = ICRS.get('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
     assert CHE.value.message.startswith('207 Unexpected Error: ')
 
+
 def test_wrapped_patch_success(ICRS):
     response_obj = ICRS.patch('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
+
 
 def test_wrapped_patch_207_fail(ICRS):
     ICRS.session.patch.return_value.status_code = 207
@@ -158,8 +182,10 @@ def test_wrapped_patch_207_fail(ICRS):
         response_obj = ICRS.patch('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
     assert CHE.value.message.startswith('207 Unexpected Error: ')
 
+
 def test_wrapped_post_success(ICRS):
     response_obj = ICRS.post('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
+
 
 def test_wrapped_post_207_fail(ICRS):
     ICRS.session.post.return_value.status_code = 207
@@ -167,8 +193,10 @@ def test_wrapped_post_207_fail(ICRS):
         response_obj = ICRS.post('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
     assert CHE.value.message.startswith('207 Unexpected Error: ')
 
+
 def test_wrapped_put_success(ICRS):
     response_obj = ICRS.put('ltm/nat/', 'A_FOLDER_NAME', 'AN_INSTANCE_NAME') 
+
 
 def test_wrapped_put_207_fail(ICRS):
     ICRS.session.put.return_value.status_code = 207
