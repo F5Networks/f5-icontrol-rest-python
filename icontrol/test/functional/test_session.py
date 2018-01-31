7dcdad67b7d8f9ac967e54c6898f47e7476d529b
Tim Rupp <caphrim007@gmail.com>
2018-01-31 11:13:02 -0800

    Fixes api calls (#146)
    The changes to debug required that kwargs to certain commands
    be differentiated. This is an attempt at making that work

--- icontrol/test/functional/test_session.py
+++ icontrol/test/functional/test_session.py
@@ -675,3 +675,12 @@ def test_using_tmos_auth_provider(GET_URL, opt_bigip, opt_username, opt_password
     response = icr1.get(GET_URL)
     assert response.status_code == 200
     assert response.json()
+
+
+def test_debug_tracing(request, POST_URL, GET_URL, opt_bigip, opt_username, opt_password):
+    icr1 = iControlRESTSession(opt_username, opt_password, auth_provider='tmos')
+    icr1.get(GET_URL)
+    response = icr1.post(POST_URL, json=nat_data)
+    response.json()
+    teardown_nat(request, icr1, POST_URL, nat_data['name'], nat_data['partition'])
+    assert len(icr1._debug) > 0
